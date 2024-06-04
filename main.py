82c9942101aa1daebbe24bce6465498fa9937d04
Tobi Sakatoi <tobizehpanda@gmail.com>
2024-06-04 04:41:24 -0500

    expander

--- main.py
+++ main.py
@@ -16,20 +16,16 @@ st.markdown(
 badge(type="github", name="TobiZehPanda/dotfiles")
 
 st.divider()
-st.markdown(
-  """
-  ### Environment Variables
-  """
-)
-col1, col2 = st.columns(2)
+with st.expander("Environment Variables"):
+  col1, col2 = st.columns(2)
 
-with col1:
-  for var in [ "DISPLAY", "EDITOR", "HISTFILE", "HOME", "LANG", "SHELL"]:
-    if var in os.environ:
-      st.write(f"{var}: :orange[{os.environ[var]}]")
+  with col1:
+    for var in [ "DISPLAY", "EDITOR", "HISTFILE", "HOME", "LANG", "SHELL"]:
+      if var in os.environ:
+        st.write(f"{var}: :orange[{os.environ[var]}]")
 
-with col2:
-  for var in ["TERM", "USER", "XDG_BACKEND", "XDG_CONFIG_DIR", "XDG_CURRENT_DESKTOP", "XDG_SESSION_TYPE"]:
-    if var in os.environ:
-      st.write(f"{var}: :orange[{os.environ[var]}]")
+  with col2:
+    for var in ["TERM", "USER", "XDG_BACKEND", "XDG_CONFIG_DIR", "XDG_CURRENT_DESKTOP", "XDG_SESSION_TYPE"]:
+      if var in os.environ:
+        st.write(f"{var}: :orange[{os.environ[var]}]")
 
